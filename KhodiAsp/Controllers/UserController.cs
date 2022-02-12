caff4a7fb624544b64e5bb0a3650d3d1e243ca32
Brian Kiwaa <briankiwaa@outlook.com>
2022-02-12 23:53:21 +0300

    Add user method

---
+++ KhodiAsp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using KhodiAsp.Models;
+using KhodiAsp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace KhodiAsp.Controllers
+{
+    public class UserController : ApiController
+    {
+        private readonly UserRepository userRepo = new UserRepository();
+
+        [HttpPost]
+        [Route("api/users/register")]
+        [ResponseType(typeof(Users))]
+        public async Task<IHttpActionResult> registerUser(Users user)
+        {
+            var data = await userRepo.createUser(user);
+
+            return data != null ? 
+                (IHttpActionResult)Ok(data) : BadRequest();
+        }
+    }
+}
---
+++ KhodiAsp/Repositories/UserRepository.cs
@@ -0,0 +1,66 @@
+﻿using KhodiAsp.Models;
+using KhodiAsp.Security;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace KhodiAsp.Repositories
+{
+
+    interface iUserRepository
+    {
+        Task<Users> createUser(Users user);
+
+        Task<bool> forgotPassword(string email);
+
+        Task<string> loginUser(string username, string password);
+    }
+
+    public class UserRepository : iUserRepository
+    {
+        khodi_ef db = new khodi_ef();       
+
+        public UserRepository()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public async Task<Users> createUser(Users user)
+        {
+            try
+            {
+                var passWord = PasswordHasher.createHash(user.password);
+                user.password = passWord;
+                user.createdAt = DateTime.UtcNow;
+                user.updatedAt = DateTime.UtcNow;
+                user.enabled = false;
+                user.accountExpired = false;
+                user.credentialsExpired = false;
+                user.userId = Guid.NewGuid();
+
+                db.users.Add(user);
+                await db.SaveChangesAsync();
+
+                user.password = "-------";
+                return user;
+            }catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public Task<bool> forgotPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> loginUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
