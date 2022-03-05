de810c4852c13a3b1b2b71f1c6344427eaa1d132
Brian Kiwaa <briankiwaa@outlook.com>
2022-03-05 15:48:00 +0300

    -Add asset repository -Add asset controller

---
+++ KhodiAsp/Controllers/AssetsController.cs
@@ -0,0 +1,54 @@
+﻿using KhodiAsp.Data;
+using KhodiAsp.Models;
+using KhodiAsp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace KhodiAsp.Controllers
+{
+    public class AssetsController : ApiController
+    {
+        readonly AssetsRepository aRepo = new AssetsRepository();
+
+
+        [Authorize]
+        [HttpPost]
+        [Route("api/propertyAssets/addPropertyAsset")]
+        [ResponseType(typeof(Response<Assets>))]
+        public async Task<IHttpActionResult> addProperyAsset(Guid propertyId, Assets asset)
+        {
+            var data = await aRepo.addPropertyAsset(propertyId, asset);
+
+            return Ok(data);
+        }
+
+        [Authorize]
+        [HttpPatch]
+        [Route("api/propertyAssets/updatePropertyAssets")]
+        [ResponseType(typeof(Response<Assets>))]
+        public async Task<IHttpActionResult> updatePropertyAsset(Assets asset)
+        {
+            var data = await aRepo.updatePropertyAsset(asset);
+
+            return Ok(data);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/properyAssets/getPropertyAssets")]
+        [ResponseType(typeof(Response<List<Assets>>))]
+        public IHttpActionResult getPropertyAssets(string propertyId)
+        {
+            var data = aRepo.getProperyAssets(propertyId);
+
+            return Ok(data);
+        }
+
+    }
+}
---
+++ KhodiAsp/Repositories/AssetsRepository.cs
@@ -0,0 +1,108 @@
+﻿using KhodiAsp.Data;
+using KhodiAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace KhodiAsp.Repositories
+{
+    interface iAssetsRepository
+    {
+        Task<Response<Assets>> addPropertyAsset(Guid properyId, Assets asset);
+        Task<Response<Assets>> updatePropertyAsset(Assets asset);
+        Response<List<Assets>> getProperyAssets(string properyId);
+        Task<Response<bool>> deleteProperyAsset(Guid assetId);
+    }
+
+    public class AssetsRepository : iAssetsRepository
+    {
+        khodi_ef db = new khodi_ef();
+
+        async public Task<Response<Assets>> addPropertyAsset(Guid properyId, Assets asset)
+        {
+            var response = new Response<Assets>();
+            try
+            {
+                asset.assetId = Guid.NewGuid();
+                asset.createdAt = DateTime.UtcNow;
+                asset.updatedAt = DateTime.UtcNow;
+                asset.propertyId = properyId.ToString();
+
+                db.assets.Add(asset);
+
+                await db.SaveChangesAsync();
+
+                response.responseMessage = ResponseMessages.SUCCESS;
+                response.response = asset;
+
+            }catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                response.responseMessage = ResponseMessages.ERROR;
+                response.response = null;
+            }
+            return response;
+        }
+
+        async public Task<Response<bool>> deleteProperyAsset(Guid assetId)
+        {
+            var response = new Response<bool>();
+            try
+            {
+                var asset = db.assets.Where(a => a.assetId == assetId).FirstOrDefault();
+
+                db.assets.Remove(asset);
+
+                await db.SaveChangesAsync();
+
+                response.responseMessage = ResponseMessages.SUCCESS;
+                response.response = true;
+
+            }catch(Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+                response.responseMessage = ResponseMessages.ERROR;
+                response.response = false;
+            }
+            return response;
+        }
+
+        public Response<List<Assets>> getProperyAssets(string properyId)
+        {
+            var response = new Response<List<Assets>>();
+            var assets = db.assets.Where(a => a.propertyId == properyId).ToList();
+
+            response.responseMessage = ResponseMessages.SUCCESS;
+            response.response = assets;
+
+            return response; 
+        }
+
+        async public Task<Response<Assets>> updatePropertyAsset(Assets asset)
+        {
+            var response = new Response<Assets>();
+            try
+            {         
+                asset.updatedAt = DateTime.UtcNow;
+
+                db.Entry(asset).State = System.Data.Entity.EntityState.Modified;
+
+                await db.SaveChangesAsync();
+
+                response.responseMessage = ResponseMessages.SUCCESS;
+                response.response = asset;
+
+            }catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                response.responseMessage = ResponseMessages.ERROR;
+                response.response = null;
+            }
+            return response;
+        }
+    }
+}
