be2a74771781f25b56f62a8de4f7f526dece574c
Brian Kiwaa <briankiwaa@outlook.com>
2022-08-26 19:11:19 +0300

    db migrations and units changes

---
+++ KhodiAsp/Migrations/202207041823138_update_units.cs
@@ -0,0 +1,24 @@
+﻿namespace KhodiAsp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update_units : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Units", "deposit", c => c.Single(nullable: false));
+            AddColumn("dbo.Units", "numOfRooms", c => c.Int(nullable: false));
+            AddColumn("dbo.Units", "blockNumber", c => c.String());
+            AddColumn("dbo.Units", "numOfBedrooms", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Units", "numOfBedrooms");
+            DropColumn("dbo.Units", "blockNumber");
+            DropColumn("dbo.Units", "numOfRooms");
+            DropColumn("dbo.Units", "deposit");
+        }
+    }
+}
--- KhodiAsp/Models/Units.cs
+++ KhodiAsp/Models/Units.cs
@@ -6,12 +6,17 @@ using System.Web;
 
 namespace KhodiAsp.Models
 {
+    //Specify Block(If it exists),House Number, Deposit, Rent Amount, No of rooms, Drop Down House Type(Ensuite, Studio/Bedsitter, 1,2,3,4,5 Bedroom
     public class Units
     {
         [Key]
         public Guid unitId { get; set; }
         public float maxRent { get; set; }
         public float minRent { get; set; }
+        public float deposit { get; set; }
+        public int numOfRooms { get; set; }
+        public string blockNumber { get; set; }
+        public int numOfBedrooms { get; set; }
         public string unitNnumber { get; set; }
         public DateTime createdAt { get; set; }
         public DateTime updatedAt { get; set; } 
